40b8a70fadd34671be2eb4ddcd095b04a06cee50
KArathorn <42720532+KArathorn@users.noreply.github.com>
2018-08-30 22:35:58 +0200

    QV
    QV

--- resources/script.js
+++ resources/script.js
@@ -1,10 +1,9 @@
-var config={};
-Playground.authenticate(config, "qsocks").then(function(ticket){
-  config.ticket = ticket;
-  qsocks.ConnectOpenApp(config).then(function(result){
-    var global = result[0];
-    var app = result[1];
-    console.log(global);
-    console.log(app);
-  });
-});
+var config={
+	host:"playground.qlik.com",
+	prefix:"/playground/",
+	port:"443",
+	isSecure:true,
+	rejectUnauthorized:false,
+	apiKey:"Z3ZBpWml2kNS4jPxELTFIAwP1efGQWH1",
+	appname:"5cdb3471-892c-4495-93cd-9f26f7eca41c"
+};
